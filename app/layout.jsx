5ddc498413150f2cebc90d4df0ab8693f7e59741
Vineet <infinite2202@gmail.com>
2024-08-09 12:43:09 +0530

    page transition

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,12 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
 
 const jetbrainsMono = JetBrains_Mono({
-   subsets: ["latin"],
-   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
-   variable:"--font-jetbrainsMono",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-jetbrainsMono",
 });
 
 export const metadata = {
@@ -18,8 +19,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={jetbrainsMono.variable}>
         <Header />
-        {children}
-        </body>
+        <PageTransition>{children}</PageTransition>
+      </body>
     </html>
   );
 }

52ebca7abc0442befa4992481d253233964bb206
Vineet <infinite2202@gmail.com>
2024-08-11 10:24:08 +0530

    contact info

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -5,7 +5,25 @@ import { Input } from "@/components/ui/input"
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Textarea } from "@/components/ui/textarea"
 
-import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa"
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa"
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "(+91) 9120508661"
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "vineetprajapati2202@gmail.com"
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Address",
+    description: "Availabel 24/7 Online"
+  },
+]
 
 const Contact = () => {
   return (

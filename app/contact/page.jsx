f15dc769e8ab510a05ab8e1d4bec5d0c8d24a59e
Vineet <infinite2202@gmail.com>
2024-08-11 10:19:45 +0530

    all imports

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+"use state"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { Textarea } from "@/components/ui/textarea"
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa"
 
 const Contact = () => {
   return (
@@ -7,3 +14,4 @@ const Contact = () => {
 }
 
 export default Contact
+

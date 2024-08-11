66b73e00c35ac55e249540f5877632400b9a7927
Vineet <infinite2202@gmail.com>
2024-08-11 10:32:35 +0530

    forget to add use clietn instead add use state

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,35 +1,61 @@
-"use state"
+"use client";
 
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { motion } from "framer-motion";
 
-import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa"
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 
 const info = [
   {
     icon: <FaPhoneAlt />,
     title: "Phone",
-    description: "(+91) 9120508661"
+    description: "(+91) 9120508661",
   },
   {
     icon: <FaEnvelope />,
     title: "Email",
-    description: "vineetprajapati2202@gmail.com"
+    description: "vineetprajapati2202@gmail.com",
   },
   {
     icon: <FaMapMarkerAlt />,
     title: "Address",
-    description: "Availabel 24/7 Online"
+    description: "Availabel 24/7 Online",
   },
-]
+];
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* form */}
+          <div>
+            <form>form</form>
+          </div>
+          {/* info */}
+          <div>info</div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
 
-export default Contact
-
+export default Contact;

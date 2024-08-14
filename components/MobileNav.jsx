8777262974dba8d15006bf875038ba36bcab29fd
Vineet <infinite2202@gmail.com>
2024-08-14 13:10:29 +0530

    close trigger added for mobile devices

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "./ui/sheet";
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
 
@@ -39,18 +39,29 @@ const MobileNav = () => {
       <SheetContent className="flex flex-col">
         <div className="mt-32 mb-40 text-center text-2xl">
           <Link href="/">
-            <h1 className="text-4xl font-semibold">
-              Vineet<span className="text-accent">.</span>
-            </h1>
+            <SheetClose>
+              <h1 className="text-4xl font-semibold">
+                Vineet<span className="text-accent">.</span>
+              </h1>
+            </SheetClose>
           </Link>
         </div>
 
         <nav className="flex flex-col justify-center items-center gap-8">
           {links.map((link, index) => {
             return (
-              <Link href={link.path} key={index} className={`${link.path === pathname && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all`}>
-                {link.name}
-              </Link>
+              <>
+                <Link
+                  href={link.path}
+                  key={index}
+                  className={`${
+                    link.path === pathname &&
+                    "text-accent border-b-2 border-accent"
+                  } text-xl capitalize hover:text-accent transition-all`}
+                >
+                  <SheetClose>{link.name}</SheetClose>
+                </Link>
+              </>
             );
           })}
         </nav>
